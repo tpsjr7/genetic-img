4f5f793d77071d39b97b1551789d2a4b680fb8c7
Ted Sanders <tpsjr7@gmail.com>
2022-05-15 15:13:30 -0700

    move to asserts.js file

--- push-test.js
+++ push-test.js
@@ -1,49 +1,4 @@
 
-
-function makeRandomSeq(seq){
-  let count = 0;
-  return function() {
-    if (count >= seq.length){
-      throw "out of range";
-    }
-    return seq[count++];
-  };
-}
-
-function assertEquals(expected, actual, message) {
-  if (typeof expected != typeof actual) {
-    throw `Fail: types dont match. Expected: ${expected} vs Actual: ${actual}`;
-  }
-  if (Array.isArray(expected) && Array.isArray(actual)) {
-    if (expected.length != actual.length || expected.toString() !== actual.toString()) {
-      throw `Failed. Excepted: ${expected}, Actual: ${actual}`;
-    }
-    return;
-  }
-  if (expected !== actual) {
-    let mess = message ?  message + ', ' : '';
-    mess += 'Failed: Expected: ' + expected + ', Actual: ' + actual;
-    throw mess;
-  }
-}
-function assertTrue(expression) {
-  if (!expression) {
-    throw "not true";
-  }
-}
-function assertThrows(code) {
-  try {
-    code();
-
-  } catch (e) {
-    return;
-  }
-  throw "Fail, didn't throw as expected";
-}
-
-function fail(mess) {
-  throw "Fail " + mess;
-}
 let tests = {
   testAssertEquals() {
     assertEquals([0], [0]);
