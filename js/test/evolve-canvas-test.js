23afdf428c0ffbf34a006a9f790d019520f9dee5
Ted Sanders <tpsjr7@gmail.com>
2022-05-15 18:54:13 -0700

    fix test

--- js/test/evolve-canvas-test.js
+++ js/test/evolve-canvas-test.js
@@ -5,12 +5,12 @@ addTests({
       innerWidth: 1600
     };
     let cm = new CanvasManager(window, 15);
-    assertEquals({x: 0, y: 0}, cm.calcPositionForCanvas(0));
-    assertEquals({x: 400, y: 0}, cm.calcPositionForCanvas(1));
-    assertEquals({x: 800, y: 0}, cm.calcPositionForCanvas(2));
-    assertEquals({x: 1200, y: 0}, cm.calcPositionForCanvas(3));
-    assertEquals({x: 0, y: 400}, cm.calcPositionForCanvas(4));
-    assertEquals({x: 400, y: 400}, cm.calcPositionForCanvas(5));
-    assertEquals({x: 800, y: 1200}, cm.calcPositionForCanvas(14));
+    assertEquals({x: 0, y: 0, width:400, height:400}, cm.calcPositionForCanvas(0));
+    assertEquals({x: 400, y: 0, width:400, height:400}, cm.calcPositionForCanvas(1));
+    assertEquals({x: 800, y: 0, width:400, height:400}, cm.calcPositionForCanvas(2));
+    assertEquals({x: 1200, y: 0, width:400, height:400}, cm.calcPositionForCanvas(3));
+    assertEquals({x: 0, y: 400, width:400, height:400}, cm.calcPositionForCanvas(4));
+    assertEquals({x: 400, y: 400, width:400, height:400}, cm.calcPositionForCanvas(5));
+    assertEquals({x: 800, y: 1200, width:400, height:400}, cm.calcPositionForCanvas(14));
   }
 });
